39d73c8efd62e33a45b69867f75b9b8a64853407
march-dave <ysangy@gmail.com>
2017-09-17 14:38:03 -0400

    up to date

--- routes/twillo.js
+++ routes/twillo.js
@@ -25,47 +25,47 @@ router.post("/", (req, res) => {
     });
 
 
-    var options = {
-        method: 'POST',
-        url: 'https://api-dev.fareportallabs.com/air/api/search/searchflightavailability',
-        headers: {
-            'postman-token': '00950b9f-68f4-4fe2-0dfe-6a347d3deada',
-            'cache-control': 'no-cache',
-            authorization: 'Basic eXNhbmd5QGdtYWlsLmNvbTpDNjcwMjc0OQ==',
-            'content-type': 'application/json'
-        },
-        body: {
-            FlightSearchRequest: {
-                Adults: 1,
-                Child: 0,
-                Seniors: 0,
-                InfantInLap: 0,
-                InfantOnSeat: 0,
-                Youths: 0,
-                Kid: 0,
-                TypeOfTrip: 'ONEWAYTRIP',
-                ClassOfService: 'ECONOMY',
-                SearchAlternateDates: false,
-                FromTime: null,
-                ToTime: null,
-                SegmentDetails: [{
-                    Origin: 'NYC',
-                    Destination: 'LON',
-                    DepartureDate: '2017-10-16T00:00:00',
-                    DepartureTime: '1100'
-                }]
-            },
-            ResponseVersion: 'VERSION41'
-        },
-        json: true
-    };
+    // var options = {
+    //     method: 'POST',
+    //     url: 'https://api-dev.fareportallabs.com/air/api/search/searchflightavailability',
+    //     headers: {
+    //         'postman-token': '00950b9f-68f4-4fe2-0dfe-6a347d3deada',
+    //         'cache-control': 'no-cache',
+    //         authorization: 'Basic eXNhbmd5QGdtYWlsLmNvbTpDNjcwMjc0OQ==',
+    //         'content-type': 'application/json'
+    //     },
+    //     body: {
+    //         FlightSearchRequest: {
+    //             Adults: 1,
+    //             Child: 0,
+    //             Seniors: 0,
+    //             InfantInLap: 0,
+    //             InfantOnSeat: 0,
+    //             Youths: 0,
+    //             Kid: 0,
+    //             TypeOfTrip: 'ONEWAYTRIP',
+    //             ClassOfService: 'ECONOMY',
+    //             SearchAlternateDates: false,
+    //             FromTime: null,
+    //             ToTime: null,
+    //             SegmentDetails: [{
+    //                 Origin: 'NYC',
+    //                 Destination: 'LON',
+    //                 DepartureDate: '2017-10-16T00:00:00',
+    //                 DepartureTime: '1100'
+    //             }]
+    //         },
+    //         ResponseVersion: 'VERSION41'
+    //     },
+    //     json: true
+    // };
 
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
+    // request(options, function(error, response, body) {
+    //     if (error) throw new Error(error);
 
-        // console.log(body);
-        res.send(body);
-    });
+    //     // console.log(body);
+    //     res.send(body);
+    // });
 
 
 
